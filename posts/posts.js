e823c82bd6f88bc55712d1bb7349a785aa0908f6
Redus Rira <redis123445@gmail.com>
2020-01-04 17:15:44 +0100

    aaç

--- posts/posts.js
+++ posts/posts.js
@@ -3,9 +3,11 @@ const btn = document.getElementById("top");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      btn.style.opacity = 1;
-    } else {
-      btn.style.opacity = 0;
-    }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    btn.style.opacity = 1;
+  } else {
+    btn.style.opacity = 0;
   }
+}
+
+setTimeout(() => document.querySelector("body").style.overflow = "auto", 1000);
